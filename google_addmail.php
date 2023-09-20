cb8661a9054a41f459052040fe433dd2b8ae3d7c
Miha Šafranko <miha.safranko@gmail.com>
2023-09-20 21:15:39 +0200

    fixed google link form

--- google_addmail.php
+++ google_addmail.php
@@ -38,7 +38,7 @@
     <p>Poveži svoj trenutni račun, z svojim google računom za lažjo prijavo:</p>
  <form action="googlelink.php" method="post">
  <label for="email">Mail:</label>
-  <input type="text" id="email" name="email" required disabled value = "<?php echo $_SESSION['mail'] ?>"><br><br>
+  <input type="text" id="email" name="email" required readonly value = "<?php echo $_SESSION['mail'] ?>"><br><br>
   <label for="geslo">Geslo:</label>
   <input type="password" id="geslo" name="geslo" required><br><br>
   <input type="submit" value="Pošlji">
