95855bc130721be7c598eda725359f8b551aeccb
Miha Šafranko <miha@safko.eu>
2023-09-28 09:06:12 +0200

    Update odjava.php

--- odjava.php
+++ odjava.php
@@ -1,23 +1,26 @@
 <?php
 session_start();
-if(!isset($_SESSION['id'])){
+
+if (!isset($_SESSION['id'])) {
     setcookie('prijava', "Opla. Zakaj bi se odjavil, če pa nisi prijavljen?");
     setcookie('warning', 1);
     header('Location: index.php');
     exit();
-  }
-
-if(isset($_COOKIE['banned'])){
-    session_destroy();
-    setcookie('prijava', "Vaš račun je blokiran.");
-    setcookie('error', 1);
-    header('Location: index.php');
 }
 
+if (isset($_COOKIE['banned'])) {
+    // User is banned, show a message or redirect to a ban page
+    setcookie('prijava', "Vaš račun je blokiran.");
+    setcookie('error', 1);
+    header('Location: ban_page.php'); // Replace 'ban_page.php' with the actual ban page
+    exit();
+}
 
+// User is not banned, perform logout
 session_destroy();
-//delete cookie id and prijava
+// Delete cookies
 setcookie('prijava', "Odjava uspešna.");
-setcookie('good',"", time()- 3600);
-header('Location: index.php');
+setcookie('good', "", time() - 3600);
+header('Location: index.php'); // Redirect to the index page after successful logout
+exit();
 ?>
